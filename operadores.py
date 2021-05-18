bb872640931d5b136456bf28dbefe24c19c1fb69
Vinícius Teixeira <55103014+ViniciusTLR@users.noreply.github.com>
2021-05-18 16:08:18 -0300

    Update operadores.py

--- operadores.py
+++ operadores.py
@@ -1,21 +1,22 @@
-num1 = input("Primeiro numero: ")
-num2 = input("Segundo numero: ")
-soma = int(num1) + int(num2)
-print('A soma é =', soma)
+num1 = float(input("Primeiro numero: "))
+num2 = float(input("Segundo numero: "))
+soma = num1 + num2
+print('A soma de {} + {} é = {}'.format(num1, num2, soma))
 
-print("")
+print('')
 
-diferença = int(num1) - int(num2)
-print("A diferença é =", diferença)
+diferença = num1 - num2
+print('A diferença de {} - {} é = {}'.format(num1, num2, diferença))
 
-print("")
+print('')
 
-multiplicar = int(num1) * int(num2)
-print("A multiplicação é =", multiplicar)
+multiplicar = num1 * num2
+print('A multiplicação de {} * {} é = {}'.format(num1, num2, multiplicar))
 
-print("")
+print('')
 
-divisão = int(num1) / int(num2)
-print("A divisao é =", divisão)
+divisão = num1 / num2
+print('A divisao de {} / {} é = {}'.format(num1, num2, divisão))
+
 
 
