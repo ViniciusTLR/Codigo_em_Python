0999c4ef8777d3716cacbf61ee70a4794500fedb
Vinícius Teixeira <55103014+ViniciusTLR@users.noreply.github.com>
2021-06-15 11:52:10 -0300

    Update Acerte o numero.py

--- Acerte o numero.py
+++ Acerte o numero.py
@@ -1,6 +1,4 @@
-Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> from random import randint
+from random import randint
 print('***Acerte qual foi o número que o computador escolheu de 1 a 5***\n')
 aleatorio = randint(0, 5)
 
